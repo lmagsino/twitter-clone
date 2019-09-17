8cbefc804ee8006de8b6e7b1af5ed928b3eac700
Leo Magsino <leo.magsino819@gmail.com>
2019-09-17 13:40:14 +0800

    adds home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
 
   skip_before_action :require_login, :only => :index
 
+<<<<<<< HEAD
   def home; end
+=======
+  def index
+  end
+
+  def home
+    @tweets = General.all
+  end
+>>>>>>> adds home page
 
 end
