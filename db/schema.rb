5128c307132b1f995f6d23ac79ca56fb5f886f5e
Leo Magsino <leo.magsino819@gmail.com>
2019-09-17 09:22:29 +0800

    adds signup page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_234235) do
+ActiveRecord::Schema.define(version: 2019_09_17_003515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_234235) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
