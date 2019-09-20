8f6e9375a8c929ab6d0f7c8fbd1f50f6f289cadd
Leo Magsino <leo.magsino819@gmail.com>
2019-09-20 16:58:52 +0800

    fix conversation redirect page issues

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,6 +8,7 @@ class TweetsController < ApplicationController
     @tweets = Tweet.general
     @current_user_tweets = Tweet.general.by_user_id current_user.id
 
+    update_tweet_by_conversation_id
     respond_to do |format|
       format.js
     end
@@ -20,6 +21,7 @@ class TweetsController < ApplicationController
     @tweets = Tweet.general
     @current_user_tweets = Tweet.general.by_user_id current_user.id
 
+    update_tweet_by_conversation_id
     respond_to do |format|
       format.js
     end
@@ -28,7 +30,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet = Tweet.find params[:id]
     @tweet.delete
-    
+
     respond_to do |f|
       f.html { redirect_back :fallback_location => home_path }
     end
@@ -39,6 +41,10 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit :content, :tweet_id
+  end
+
+  def update_tweet_by_conversation_id
+    @tweet = Tweet.find params[:conversation_id] if params[:conversation_id]
   end
 
 end
